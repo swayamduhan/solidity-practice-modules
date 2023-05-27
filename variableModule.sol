ed9e8634da6ed2323f5ffc8f2f39c40717543a72
Swayam Duhan <130980803+swayamduhan@users.noreply.github.com>
2023-05-28 04:20:27 +0530

    Update variableModule.sol

--- variableModule.sol
+++ variableModule.sol
@@ -1,1 +1,41 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
+contract fourVars {
+    uint number;
+    string name;
+    address etherAdd;
+    bool trueOrFalse;
+
+    function setNumber(uint newNumber) public {
+        number = newNumber;
+    }
+
+    function getNumber() public view returns(uint) {
+        return number;
+    }
+
+    function setName(string memory newName) public {
+        name = newName;
+    }
+
+    function getName() public view returns(string memory) {
+        return name;
+    }
+
+    function setEtherAdd(address newAdd) public {
+        etherAdd = newAdd;
+    }
+
+    function getEtherAdd() public view returns(address) {
+        return etherAdd;
+    }
+
+    function setTrueOrFalse(bool tf) public {
+        trueOrFalse = tf;
+    }
+
+    function getTrueOrFalse() public view returns(bool) {
+        return trueOrFalse;
+    }
+}
