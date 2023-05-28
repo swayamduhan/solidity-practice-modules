83bc866d03eb721ee58efc5ef162fb538495d470
Swayam Duhan <130980803+swayamduhan@users.noreply.github.com>
2023-05-29 01:09:02 +0530

    Update sendEther.sol

--- sendEther.sol
+++ sendEther.sol
@@ -1,15 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract payment {
-    uint public valueInWei;
-    uint public valueInGwei;
-    uint public valueInEther;
+contract sendMoney {
+    address payable public recipientAddr;
+    uint public etherAmount;
+    uint public weiAmount;
+    uint public gweiAmount;
 
-    function sendMoney(uint amount) external payable {
-      
-        valueInEther = amount;
-        valueInWei = valueInEther * (10e18) ;
-        valueInGwei = valueInEther * (10e9);
+    function setRecipient(address payable addrValue) external {
+        recipientAddr = addrValue ;
     }
+
+    function sendEther() external payable {
+         recipientAddr.transfer(msg.value);
+         etherAmount = recipientAddr.balance / 1 ether;
+         weiAmount = etherAmount * (10e18);
+         gweiAmount = etherAmount * (10e9);
+    }
+
 }
